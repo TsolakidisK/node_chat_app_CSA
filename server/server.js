1e97e00588fddf2be6adc6aecf9b0b18a209a736
Tsolakidis <inf2058@hs-worms.de>
2019-02-03 20:27:59 +0100

    Added New Message and createMessage

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
     console.log('New User connected');
 
+    socket.emit('newMessage', {
+        from: 'Emely',
+        text: 'I like you',
+        createdAt: 121212
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });

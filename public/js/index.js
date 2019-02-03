efd6e87170d6d36f6edea5b91b4cfd9c96bfd392
Tsolakidis <inf2058@hs-worms.de>
2019-02-03 20:48:34 +0100

    Emit newMessage on create

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', () => {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Kwstas',
-        text: 'Yes thats fine'
-    });
 });
 
 socket.on('disconnect', () => {

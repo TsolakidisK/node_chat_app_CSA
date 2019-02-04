8ded70624e5dd943b519da40d07673253f8e2951
Tsolakidis <inf2058@hs-worms.de>
2019-02-04 23:24:01 +0100

    My current location button added

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
    it('should generate correct message object', () => {
@@ -11,4 +11,17 @@ describe('generateMessage', () => {
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Konstantin';
+        var latitude = 10;
+        var longitude = 12;
+        var url = 'https://www.google.com/maps?q=10,12';
+        var message = generateMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, text});       
+    });
 });

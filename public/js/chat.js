de6e97c0a34a4fef0dcfd5fae9f5ebbd0cf8dbfc
Tsolakidis <inf2058@hs-worms.de>
2019-02-07 13:27:40 +0100

    Added List of Users

--- public/js/chat.js
+++ public/js/chat.js
@@ -31,6 +31,16 @@ socket.on('disconnect', () => {
     console.log('Disconnected from server');
 });
 
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
+});
+
 socket.on('newMessage', function(message) {
     var formattedTime = moment(message.createdAt).format('kk:mm');
     var template = jQuery('#message-template').html();
